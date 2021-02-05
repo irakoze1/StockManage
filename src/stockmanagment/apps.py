0718043f5eec7ae7e1dca79ae7bd7b9ecc45bb62
irakoze1 <irakozetititof@gmail.com>
2021-02-05 22:29:04 +0200

    StockMangementApp

---
+++ src/stockmanagment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StockmanagmentConfig(AppConfig):
+    name = 'stockmanagment'
---
+++ src/stockmanagment/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import StockCreateForm, StockSearchForm, StockUpdateForm
+
+
+def home(request):
+    title = 'Welcome: TITITOF'
+    context = {
+        "title": title,
+    }
+    return render(request, "home.html", context)
+
+
+def list_items(request):
+    header = 'List of Items'
+    form = StockSearchForm(request.POST or None)
+    queryset = Stock.objects.all()
+    context = {
+        "header": header,
+        "queryset": queryset,
+        "form": form,
+    }
+    if request.method == 'POST':
+        queryset = Stock.objects.filter(category__icontains=form['category'].value(
+        ), item_name__icontains=form['item_name'].value())
+    context = {
+        "form": form,
+        "header": header,
+        "queryset": queryset,
+    }
+    return render(request, "list_items.html", context)
+
+
+def add_items(request):
+    form = StockCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/list_items')
+    context = {
+        "form": form,
+        "title": "Add Item",
+    }
+    return render(request, "add_items.html", context)
+
+
+def update_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    form = StockUpdateForm(instance=queryset)
+    if request.method == 'POST':
+        form = StockUpdateForm(request.POST, instance=queryset)
+        if form.is_valid():
+            form.save()
+            return redirect('/list_items')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'add_items.html', context)
+
+
+def delete_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    if request.method == 'POST':
+        queryset.delete()
+        return redirect('/list_items')
+    return render(request, 'delete_items.html')
